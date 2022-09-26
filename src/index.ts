f0afe061f95851693391e1450c43f3df6f8932c5
chepkoyallan <chepkoyallan@icloud.com>
2022-09-26 09:36:17 +0900

    Arrays

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
+// Build in types
 let sales:number = 100_000_000;
 let course: string = "TypeScript";
 let is_published: boolean = true;
+
+// The any type
 let level;
 level = 1;
 
 function render(document:any){
     console.log(document)
 }
+
+// Arrays
+let numbers: number[] = [1, 3, 4, 5]
+// numbers.forEach(n => n.)
