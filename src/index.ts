3f429e68fe7165e1bd2060c9e6c2631e3057feec
chepkoyallan <chepkoyallan@icloud.com>
2022-09-26 09:27:11 +0900

    The any type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 let sales:number = 100_000_000;
 let course: string = "TypeScript";
 let is_published: boolean = true;
+let level;
+level = 1;
+
+function render(document:any){
+    console.log(document)
+}
